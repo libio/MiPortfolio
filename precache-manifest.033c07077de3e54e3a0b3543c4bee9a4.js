faa12a818300dc86f533850883d892448e09dc63
libio <134541@unsaac.edu.pe>
2020-07-07 10:13:04 -0500

    Updates

---
+++ precache-manifest.033c07077de3e54e3a0b3543c4bee9a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b1cd2e9fe58f82fc2b803a5893ece30",
+    "url": "/MiPortfolio/index.html"
+  },
+  {
+    "revision": "c30998f6164b6e9e14e2",
+    "url": "/MiPortfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c3e93e7ad4441e3c9fc7",
+    "url": "/MiPortfolio/static/js/2.c9d6a5b5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MiPortfolio/static/js/2.c9d6a5b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c30998f6164b6e9e14e2",
+    "url": "/MiPortfolio/static/js/main.e9117f67.chunk.js"
+  },
+  {
+    "revision": "da5c0d28235d64b00de7",
+    "url": "/MiPortfolio/static/js/runtime-main.3fbed1e3.js"
+  }
+]);
